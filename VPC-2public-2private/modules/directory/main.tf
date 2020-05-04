aa3298ed345a948626077fc6f3925aea7b061a4a
Dario Vemagi <dario.vemagi@smeup.com>
2020-05-04 12:40:37 +0200

    test ds

--- VPC-2public-2private/modules/directory/main.tf
+++ VPC-2public-2private/modules/directory/main.tf
@@ -1,3 +1,29 @@
+# Modulo per la creazione di una active directory gestita 
+# prendendo i dati da un terraform root 
+
+# recupero delle info del vpc
+
 data "aws_subnet_ids" "sub_priv" {
     module.vpc.private_subnets
+}
+
+data "aws_vpc" "vpc_id" {
+    module.vpc.vpc_id
+}
+
+#
+
+resource "aws_directory_service_directory" "directory" {
+  name     = "corp.notexample.com"
+  password = "SuperSecretPassw0rd"
+  size     = "Small"
+
+  vpc_settings {
+    vpc_id     = "${aws_vpc.vpc_id.id}"
+    subnet_ids = ["${aws_subnet.sub_priv.id[0],aws_subnet.sub_priv.id[1]}",]
+  }
+
+  tags = {
+    Project = "tf-test-ds"
+  }
 }
