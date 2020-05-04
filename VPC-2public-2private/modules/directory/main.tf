f6d6adefc1f6664dd8f66eda262cefc230b84ddf
Dario Vemagi <dario.vemagi@smeup.com>
2020-05-04 14:54:35 +0200

    aggiunto ds

--- VPC-2public-2private/modules/directory/main.tf
+++ VPC-2public-2private/modules/directory/main.tf
@@ -1,16 +1,16 @@
 # Modulo per la creazione di una active directory gestita 
 # 
-# i parametri vpc_id e subnet_ids sono passati dal main_tf preso da main.tf chiamante
+# i parametri vpc_id e subnet_ids sono passati dal main.tf chiamante
 
-resource "aws_directory_service_directory" "directory" {
+resource "aws_directory_service_directory" "ds" {
   name     = "corp.notexample.com"
   password = "SuperSecretPassw0rd"
   edition = "Standard"
   type     = "MicrosoftAD"
 
  vpc_settings {
-    vpc_id     = "${aws_vpc.vpc_id.ids.id}"
-   subnet_ids = "${aws_subnet.sub.ids.id}"
+    vpc_id     = module.vpc_id
+    subnet_ids = module.vpc.private_subnets
 }
 
   tags = {
