db0b282a33ee4e43d07ae7323d79273521c654f5
Dario Vemagi <dario.vemagi@smeup.com>
2020-05-04 14:30:22 +0200

    prova passaggio

--- VPC-2public-2private/modules/directory/main.tf
+++ VPC-2public-2private/modules/directory/main.tf
@@ -8,10 +8,10 @@ resource "aws_directory_service_directory" "directory" {
   edition = "Standard"
   type     = "MicrosoftAD"
 
-# vpc_settings {
-#    vpc_id     = "${aws_vpc.vpc_id.ids.id}"
-#    subnet_ids = ["${aws_subnet.sub.ids.id}",]
-#  }
+ vpc_settings {
+    vpc_id     = "${aws_vpc.vpc_id.ids.id}"
+   subnet_ids = "${aws_subnet.sub.ids.id}"
+}
 
   tags = {
     Project = "tf-test-ds"
